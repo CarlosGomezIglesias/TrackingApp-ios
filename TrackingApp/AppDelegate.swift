e8eb8ae5cc122bb66af862b15af3619d87b5b198
CarlosGomezIglesias <carlos.gomez.igle@gmail.com>
2026-06-04 20:37:05 +0200

    Creado el home con su controlador el tab bar controller

--- TrackingApp/AppDelegate.swift
+++ TrackingApp/AppDelegate.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import FirebaseCore
+import GoogleSignIn
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -18,6 +19,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         FirebaseApp.configure()
         // Override point for customization after application launch.
         return true
+    }
+    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+        return GIDSignIn.sharedInstance.handle(url)
     }
 
     // MARK: UISceneSession Lifecycle
--- TrackingApp/SignInViewController.swift
+++ TrackingApp/SignInViewController.swift
@@ -7,6 +7,8 @@
 
 import UIKit
 import FirebaseAuth
+import GoogleSignIn
+import FirebaseCore
 
 class SignInViewController: UIViewController {
     //creamos las variables
@@ -23,11 +25,9 @@ class SignInViewController: UIViewController {
         if Auth.auth().currentUser != nil {
             performSegue(withIdentifier: "NavigateToHome", sender: nil)
         }
-            
+        
         
     }
-    
-    
     
     //creamos funcion para cuando pinchamos en sign in
     @IBAction func signIn(_ sender: Any) {
@@ -39,7 +39,7 @@ class SignInViewController: UIViewController {
         Auth.auth().signIn(withEmail: username, password: password) { [unowned self] authResult, error in
             //si las variables username y password no son correctas, sale el mensaje
             guard error == nil else {
-                print("Error creating user: \(error!)")
+                print("Error signing In user: \(error!)")
                 //manda un mesaje de error con la descripcion de lo ocurrido, de tipo alerta
                 let alert = UIAlertController(title: "Sign Up error", message: error!.localizedDescription,preferredStyle: .alert)
                 //se puede añadir que texto pone en el boton de la alerta
@@ -52,6 +52,58 @@ class SignInViewController: UIViewController {
             //si todo es correcto navegamos a home
             performSegue(withIdentifier: "NavigateToHome", sender: nil)
         }
+        
+    }
+    @IBAction func signInWithGoogle(_ sender: Any) {
+        guard let clientID = FirebaseApp.app()?.options.clientID else { return }
+        
+        // Create Google Sign In configuration object.
+        let config = GIDConfiguration(clientID: clientID)
+        GIDSignIn.sharedInstance.configuration = config
+        
+        // Start the sign in flow!
+        GIDSignIn.sharedInstance.signIn(withPresenting: self) {[unowned self] result, error in
+            guard error == nil else {
+                print("Error signIn with Google: \(error!)")
+                //manda un mesaje de error con la descripcion de lo ocurrido, de tipo alerta
+                let alert = UIAlertController(title: "Sign In with Google error", message: error!.localizedDescription,preferredStyle: .alert)
+                //se puede añadir que texto pone en el boton de la alerta
+                //el handler seria la funcion lambda que quieres que ocurra cuando el usuario pulse el boton Ok
+                alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+                self.present(alert, animated: true, completion: nil)
+                
+                return
+            }
+            
+            guard let user = result?.user, let idToken = user.idToken?.tokenString else {
+                print("Error getting user info with Google: \(error!)")
+                return
+            }
+            
+            let credential = GoogleAuthProvider.credential(withIDToken: idToken, accessToken: user.accessToken.tokenString)
+            //le decimos a firebase que nos loge con las credenciales
+            Auth.auth().signIn(with: credential) { [unowned self] result, error in
+                guard error == nil else {
+                    print("Error signing In user: \(error!)")
+                    //manda un mesaje de error con la descripcion de lo ocurrido, de tipo alerta
+                    let alert = UIAlertController(title: "Sign Up error", message: error!.localizedDescription,preferredStyle: .alert)
+                    //se puede añadir que texto pone en el boton de la alerta
+                    //el handler seria la funcion lambda que quieres que ocurra cuando el usuario pulse el boton Ok
+                    alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+                    self.present(alert, animated: true, completion: nil)
+                    return
+                }
+                
+                // At this point, our user is signed in
+                performSegue(withIdentifier: "NavigateToHome", sender: nil)
+            }
+        }
+    }
+    @IBAction func signInWithFacebook(_ sender: Any) {
+        
+    }
+    @IBAction func signInWithApple(_ sender: Any) {
+        
     }
 }
 
